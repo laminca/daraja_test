695cdcff9d393e0c63a350391eaccb37803f23d5
laminca <108716845+laminca@users.noreply.github.com>
2022-09-12 08:50:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,160 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Payment portal</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+   
+    <style>
+      * {
+      box-sizing: border-box;
+      }
+      html, body {
+      min-height: 100vh;
+      padding: 0;
+      margin: 0;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 14px; 
+      color: #666;
+      
+      }
+      input, textarea { 
+      outline: none;
+      }
+      body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 20px;
+      
+      }
+      h1 {
+      margin-top: 0;
+      font-weight: 500;
+      color: #32cd32;
+      }
+      form {
+      position: relative;
+      width: 80%;
+      border-radius: 30px;
+      background: #fff;
+      border: #3e4f24;
+      }
+      .form-left-decoration,
+      .form-right-decoration {
+      content: "";
+      position: absolute;
+      width: 50px;
+      height: 20px;
+      border-radius: 20px;
+      background: #32cd32;
+      }
+      .form-left-decoration {
+      bottom: 60px;
+      left: -30px;
+      }
+      .form-right-decoration {
+      top: 60px;
+      right: -30px;
+      }
+      .form-left-decoration:before,
+      .form-left-decoration:after,
+      .form-right-decoration:before,
+      .form-right-decoration:after {
+      content: "";
+      position: absolute;
+      width: 50px;
+      height: 20px;
+      border-radius: 30px;
+      background: #fff;
+      }
+      .form-left-decoration:before {
+      top: -20px;
+      }
+      .form-left-decoration:after {
+      top: 20px;
+      left: 10px;
+      }
+      .form-right-decoration:before {
+      top: -20px;
+      right: 0;
+      }
+      .form-right-decoration:after {
+      top: 20px;
+      right: 10px;
+      }
+      .circle {
+      position: absolute;
+      bottom: 80px;
+      left: -55px;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: #fff;
+      }
+      .form-inner {
+      padding: 40px;
+      }
+      .form-inner input,
+      .form-inner textarea {
+      display: block;
+      width: 100%;
+      padding: 15px;
+      margin-bottom: 10px;
+      border: none;
+      border-radius: 20px;
+      background: #d0dfe8;
+      }
+      .form-inner textarea {
+      resize: none;
+      }
+      button {
+      width: 100%;
+      padding: 10px;
+      margin-top: 20px;
+      border-radius: 20px;
+      border: none;
+      border-bottom: 4px solid #3e4f24;
+      background: #32cd32; 
+      font-size: 16px;
+      font-weight: 400;
+      color: #fff;
+      }
+      button:hover {
+      background: #3e4f24;
+      } 
+      @media (min-width: 568px) {
+      form {
+      width: 60%;
+      }
+      }
+    </style>
+  </head>
+  <body>
+    
+    <form action="processing.php" class="decor" method="post">
+    <h1>Ebenezar Academy</h1>
+        <img src="/Daraja ofl/json/mpesa_express/pngwing.com.png" alt="" height="300" width="500">
+        
+      <div class="form-left-decoration"></div>
+      <div class="form-right-decoration"></div>
+      <div class="circle"></div>
+      <div class="form-inner">
+        <h2>Pay Fees Here</h2>
+        
+        <label for="">Enter phone number...</label>
+        <input type="text" name="number" placeholder="254....">
+        <label for="">Student Admission number...</label>
+        <input type="text" name="admno" placeholder="Admission number...">
+        <label for="">Amount...</label>
+        <input type="number" name="amount" id="">
+        
+
+        <button type="submit" name="submit" value="submit">Submit</button>
+   
+      </div>
+    </form>
+    
+  </body>
+</html>
---
+++ processing.php
@@ -0,0 +1,109 @@
+<?php
+
+if($_POST['submit']=='submit'){
+
+    pay();
+}
+else{
+    echo "error";
+}
+
+function pay(){
+$phonenumber = $_POST['number'];
+$admno=$_POST['admno'];
+
+$url='https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$ch = curl_init();
+curl_setopt($ch , CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER,  ['Authorization: Basic ' . base64_encode('cXlVra0UqXuhKGtAZONqjVbOLhvtyIHn:3GEVyIhsgby98r1w')]);
+//['Authorization: Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==']
+//['Authorization: Bearer cFJZcjZ6anEwaThMMXp6d1FETUxwWkIzeVBDa2hNc2M6UmYyMkJmWm9nMHFRR2xWOQ==']
+// ['Authorization: Basic ' . base64_encode('cXlVra0UqXuhKGtAZONqjVbOLhvtyIHn:3GEVyIhsgby98r1w')]
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+
+if($e =curl_error($ch)){
+
+    echo $e;
+}
+else
+{
+    $json = json_decode($response,1);
+    print_r($json);
+    echo $response."<br>";
+    $access_token=$json['access_token'];
+
+    
+
+
+
+}
+
+curl_close($ch);
+
+
+
+
+
+
+
+
+
+
+
+date_default_timezone_set('Africa/Nairobi');
+$timestamp = date('YmdHis');
+
+$BusinessShortCode = 174379;
+$passkey='bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+$password = base64_encode($BusinessShortCode.$passkey.$timestamp);
+$partyA=$phonenumber;
+$amount=$_POST['amount'];
+
+//$access_token_url;
+//$initiate_url;
+
+
+$url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+$callback_url='https://webhook.site/ca8f670e-11e1-4b1a-9148-59f7e2518765/';
+$stkheaders =['Content-Type: application/json','Authorization: Bearer '.$access_token];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$stkheaders);
+
+
+$data = array(
+    "BusinessShortCode" => 174379,
+    "Password" => $password, "Timestamp" => $timestamp, "TransactionType" => 'CustomerPayBillOnline', 'Amount' => $amount,
+    'PartyA' => $partyA, 'PartyB' => 174379, 'PhoneNumber' =>$partyA, 'CallBackURL' => 'https://webhook.site/ca8f670e-11e1-4b1a-9148-59f7e2518765/',
+    'AccountReference' => $admno, 'TransactionDesc' => 'Payment for fees for Student '.$admno.' in account number Ebenezar Academy'
+);
+$json = json_encode( $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch,CURLOPT_POST,true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$json);
+$response= curl_exec($ch);
+
+if($e =curl_error($ch)){
+
+    echo $e;
+}
+else
+{
+    $json = json_decode($response,1);
+    echo $response."<br>";
+    echo $json['ResponseDescription'];
+   
+
+    
+
+
+}
+curl_close($ch);
+
+
+
+}
+
+
+?>
